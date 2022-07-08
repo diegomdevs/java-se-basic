5cf724450baf1921ace33c2653f3775d29ed5bcc
Diego <diegom.develops@gmail.com>
2022-07-07 22:36:08 -0400

    feat: Variables
    Learning how to create variables and creating my first variables in Java :)

--- variables/src/Variables.java
+++ variables/src/Variables.java
@@ -1,3 +1,18 @@
 public class Variables {
-    
+    public static void main(String[] args) {
+        // Stating a variable.
+        int speed;
+
+        // Assigning a value to a variable.
+        speed = 10;
+
+        int salary = 1000;
+
+        String employer_name = "Felix";
+
+        // Printing variables data.
+
+        System.out.println("Speed value: " + speed);
+        System.out.println("Employer name: " + employer_name);
+    }
 }
