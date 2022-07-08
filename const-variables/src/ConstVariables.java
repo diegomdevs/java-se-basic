22c0f0ce6ce75202a8538cd2a2e1d67f23e38d2c
Diego <diegom.develops@gmail.com>
2022-07-07 23:41:54 -0400

    feat: Variables
    Creating and naming constant variables in Java

---
+++ const-variables/src/ConstVariables.java
@@ -0,0 +1,13 @@
+public class ConstVariables {
+    public static void main(String[] args) {
+        // Stating const Variables.
+
+        // Constant variable names are usually written in uppercase
+        // and if they have more than one word they are separated
+        // with _.
+        // To create a constant variable we have to use final keyboard.
+        final int POSITION = -5;
+        final int MAX_WIDTH = 9999;
+        final int MIN_WIDTH = 1;
+    }
+}
