e94260fdc04c988cab9381ec46f1c60fe9a705ec
Diego <diegom.develops@gmail.com>
2022-07-08 11:52:45 -0400

    feat: Data Types
    Leaning Numeric Data Types

---
+++ numeric-data-types/src/NumericDataTypes.java
@@ -0,0 +1,27 @@
+public class NumericDataTypes {
+    public static void main(String[] args) {
+        // Stating Variables
+
+        // Integer
+
+        // byte is limited to three numbers.
+        byte nB = 123;
+
+        // short is limited to five numbers.
+        short nS = 12345;
+
+        // int is limited to ten numbers.
+        int n = 1234567890;
+
+        // log is limited to nineteen numbers.
+        long nL = 1234567890121231233L;
+
+        // Float
+
+        // float
+        float nF = 123.456F;
+
+        // double
+        double nD = 123.321313213213;
+    }
+}
