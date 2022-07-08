750e319cd34c0ca222c279ee607de196346b4132
Diego <diegom.develops@gmail.com>
2022-07-07 23:49:12 -0400

    feat: Variables
    Learning how to name classes and  interfaces with UpperCamelCase and  methods and variables with lowerUpperCase

---
+++ camel-case/src/CamelCase.java
@@ -0,0 +1,10 @@
+//           UpperCamelCase
+public class CamelCase {
+    public static void main(String[] args) {
+        // Stating Variables.
+
+        // lowerUpperCase
+        String fullName = "Diego Miguel Gonzalez Calero";
+        int sizeInCentimeters = 26;
+    }
+}
