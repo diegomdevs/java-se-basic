fe11e7efa5f505fbab950ac6a2a448198df1535f
Diego <diegom.develops@gmail.com>
2022-07-08 22:09:24 -0400

    feat: Logic Operators
    Using logic operators

---
+++ logic-operators/src/LogicOperators.java
@@ -0,0 +1,15 @@
+public class LogicOperators {
+    public static void main(String[] args) {
+        byte a = 8, b = 5;
+
+        // Assignment operators
+        System.out.println("Is a equal to b? -> " + (a == b));
+        System.out.println("Is a different to b? -> " + (a != b));
+        
+        // Relational operators
+        System.out.println("Is a greater to b? -> " + (a > b));
+        System.out.println("Is a lower to b? -> " + (a < b));
+        System.out.println("Is a greater or equal to b? -> " + (a >= b));
+        System.out.println("Is a lower or equal to b? -> " + (a <= b));
+    }
+}
